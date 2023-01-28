65f6d1f1f1bb55170acaf554f38123fb095a1afa
ShamimHossain1 <beastb697@gmail.com>
2023-01-28 20:45:40 +0600

    first commit

---
+++ even.js
@@ -0,0 +1,5 @@
+var even = 0;
+while( even <=100){
+    console.log(even);
+    even = even + 2;
+}
---
+++ while.js
@@ -0,0 +1,6 @@
+var roast = 1;
+while( roast <=10){
+    console.log('Roast khabo');
+    console.log(roast);
+    roast++
+}
