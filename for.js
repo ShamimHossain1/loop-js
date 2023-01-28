bf36dda47065484a6b03a20e46b82005d7f7504a
ShamimHossain1 <beastb697@gmail.com>
2023-01-28 20:59:15 +0600

    for loop

---
+++ for.js
@@ -0,0 +1,5 @@
+
+
+for (var number=0; number <= 100; number++    ){
+    console.log(number);
+}
---
+++ odd.js
@@ -0,0 +1,5 @@
+var even = 1;
+while( even <=100){
+    console.log(even);
+    even = even + 2;
+}
